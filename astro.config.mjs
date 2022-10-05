de543dd260a5ccc654de9f1cc4c2358ce1da4593
fgbyte <garciabayonf@gmail.com>
2022-10-04 22:11:11 -0400

    testing change url astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
+  site: 'https://fgbyte.vercel.app',
   integrations: [mdx(), sitemap(), tailwind()],
   output: "server",
   adapter: vercel()
