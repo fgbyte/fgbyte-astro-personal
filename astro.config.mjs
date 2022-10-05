a6174a98d4981ad7aa6a3dfa57936bed0bc42d60
fgbyte <garciabayonf@gmail.com>
2022-10-04 22:16:30 -0400

    testing [update]

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fgbyte.vercel.app',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  adapter: vercel()
+  // output: "server",
+  // adapter: vercel()
 });
