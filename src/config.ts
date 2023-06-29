8a9ca73674db196f3cfebaad35400fa8a11f8107
fgbyte <fer.garciabayon@gmail.com>
2023-06-29 13:37:45 -0400

    update site title and description

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Fernando Garcia - Freelancer 💻developer, 🎨designer';
-export const SITE_DESCRIPTION = 'TypeScript enthusiast, UX-UI envolved';
+export const SITE_TITLE = 'Fernando Garcia - Frontend Developer | Freelancer';
+export const SITE_DESCRIPTION = 'Services: Frontend Web Development, Data Collecting, Custom Automation';
