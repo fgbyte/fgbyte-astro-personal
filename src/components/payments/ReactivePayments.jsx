7e377a5c38db6c79b6642c604d38a0bad89d4724
fgbyte <fer.garciabayon@gmail.com>
2023-07-04 22:53:00 -0400

    improve payments components

--- src/components/payments/ReactivePayments.jsx
+++ src/components/payments/ReactivePayments.jsx
@@ -5,33 +5,20 @@ import { useState } from "react";
 const ReactivePayments = ({ data }) => {
   const [qrData, setQrData] = useState({ img: "", label: "", value: "" });
 
+  const processData = Object.values(data);
+
   return (
     <>
       <Modal qrData={qrData} />
-      <Payment
-        img={data.btc.img}
-        label={data.btc.label}
-        value={data.btc.value}
-        setQrData={(v) => setQrData(v)}
-      />
-      <Payment
-        img={data.usdt.img}
-        label={data.usdt.label}
-        value={data.usdt.value}
-        setQrData={(v) => setQrData(v)}
-      />
-      <Payment
-        img={data.cup.img}
-        label={data.cup.label}
-        value={data.cup.value}
-        setQrData={(v) => setQrData(v)}
-      />
-      <Payment
-        img={data.mlc.img}
-        label={data.mlc.label}
-        value={data.mlc.value}
-        setQrData={(v) => setQrData(v)}
-      />
+      {processData.map((item) => (
+        <Payment
+          key={item.label}
+          img={item.img}
+          label={item.label}
+          value={item.value}
+          setQrData={(v) => setQrData(v)}
+        />
+      ))}
     </>
   );
 };
